227e06eb674cfc85d2b77e4e13d9bd0a8780de23
Hiroshi Miura <miurahr@linux.com>
2015-06-18 07:40:18 +0900

    install requirements in setup.py
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt", session = False)
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="markedit",
     author="Hiroshi Miura",
     author_email="miurahr@linux.com",
-    version='0.2.dev2',
+    version='0.2.dev3',
     description="markedit library for django integration",
     url="https://github.com/miurahr/python-markedit/",
     packages=['markedit'],
+    install_requires = reqs,
     include_package_data=True,
     classifiers=(
         "Development Status :: 3 - Alpha",
